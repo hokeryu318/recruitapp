711313880a2f8bafb99f9b6f64f446352d7a1f25
Hoke ryu <hokelucpy@hotmail.com>
2018-11-08 20:34:37 +0800

    Session flow once

--- app/src/main/java/recruit/aidiot/com/recruit/Fragments/Session/FragmentSessionMessage.java
+++ app/src/main/java/recruit/aidiot/com/recruit/Fragments/Session/FragmentSessionMessage.java
@@ -1,17 +1,23 @@
 package recruit.aidiot.com.recruit.Fragments.Session;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import com.shrikanthravi.chatview.data.Message;
 import com.shrikanthravi.chatview.widget.ChatView;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.w3c.dom.Text;
@@ -30,13 +36,18 @@ import recruit.aidiot.com.recruit.R;
  * Created by E on 11/7/2018.
  */
 
-public class FragmentSessionMessage extends FragmentParent implements View.OnClickListener {
+public class FragmentSessionMessage extends FragmentParent {
     View rootView;
     private ACProgressFlower dialog;
     TextView btnAction;
     ChatView chatView;
+    PopupWindow declinePopup;
 
     final Handler handler = new Handler();
+
+    ArrayList<MessageModel> messageModels = new ArrayList<>();
+    ArrayList<MessageModel> messageNews;
+    public int next_id = 0;
 
     @Nullable
     @Override
@@ -44,7 +55,7 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
         rootView = inflater.inflate(R.layout.fragment_message_session, container, false);
         dialog = new ACProgressFlower.Builder(getActivity())
                 .themeColor(getResources().getColor(R.color.colorLinebase))
-                .text("Refreshing...")
+                .text("Sending...")
                 .build();
 
         chatView = (ChatView) rootView.findViewById(R.id.chatView);
@@ -63,9 +74,7 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
                 btnAction.setText("キャンセル");
                 break;
         }
-
-        loadPreviousMessage();
-
+        runTimer();
         return rootView;
     }
 
@@ -80,16 +89,11 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
 
     public void loadNewMessage(){
         int next_id = 0;
-//        if(GD.g_selectedSession.messages.size() > 0){
-//            next_id = GD.g_selectedSession.messages.get(GD.g_selectedSession.messages.size() - 1).id;
-//        }
-        String url = GD.BASE_URL + "message?session_id" + GD.g_selectedSession.id + "&next_id=" + next_id + "&user_id" + GD.g_profile.id;
+        if(messageModels.size() > 0){
+            next_id = messageModels.get(messageModels.size() - 1).id;
+        }
+        String url = GD.BASE_URL + "message?session_id=" + GD.g_selectedSession.id + "&next_id=" + next_id + "&user_id" + GD.g_profile.id;
         new RefreshSessionApi().execute(url);
-    }
-
-    @Override
-    public void onClick(View v) {
-
     }
 
     ChatView.OnClickSendButtonListener onSend = new ChatView.OnClickSendButtonListener() {
@@ -126,18 +130,47 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
         }
     }
 
+//    public void declineAction(){
+//        SessionModel sessionModel = GD.g_selectedSession;
+//        String url = GD.BASE_URL + "session/cancel";
+//        try {
+//            JSONObject jo = new JSONObject();
+//            jo.put("user_id", GD.g_profile.id);
+//            jo.put("session_id", sessionModel.id);
+//            jo.put("reason", "");
+//            new DeclineApi().execute(url, jo.toString());
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//    }
     public void declineAction(){
-        SessionModel sessionModel = GD.g_selectedSession;
-        String url = GD.BASE_URL + "session/cancel";
-        try {
-            JSONObject jo = new JSONObject();
-            jo.put("user_id", GD.g_profile.id);
-            jo.put("session_id", sessionModel.id);
-            jo.put("reason", "");
-            new DeclineApi().execute(url, jo.toString());
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        LayoutInflater inflater = (LayoutInflater)getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View layout = inflater.inflate(R.layout.popup_comment_decline, (ViewGroup)rootView.findViewById(R.id.layout_root));
+        EditText reason = layout.findViewById(R.id.ed_decline);
+        //Get ScreenSize
+        DisplayMetrics dm = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width=dm.widthPixels;
+        int height=dm.heightPixels;
+        declinePopup = new PopupWindow(layout, width, height, true);
+        declinePopup.showAtLocation(layout, Gravity.CENTER, 0, 0);
+        declinePopup.setFocusable(true);
+
+        layout.findViewById(R.id.btn_decline_submit).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = GD.BASE_URL + "session/cancel";
+                try {
+                    JSONObject jo = new JSONObject();
+                    jo.put("user_id", GD.g_profile.id);
+                    jo.put("session_id", GD.g_selectedSession.id);
+                    jo.put("reason", reason.getText().toString());
+                    new DeclineApi().execute(url, jo.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     public void sendMessage(String msg){
@@ -150,14 +183,6 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
         } catch (JSONException e) {
             e.printStackTrace();
         }
-    }
-
-    public void loadPreviousMessage(){
-//        ArrayList<MessageModel> messageModels = GD.g_selectedSession.messages;
-//        for(int i = 0; i < messageModels.size(); i++){
-//            MessageModel messageModel = messageModels.get(i);
-//            addMessage(messageModel);
-//        }
     }
 
     public void addMessage(MessageModel messageModel){
@@ -181,6 +206,7 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
             case 3:
             case 4:
                 btnAction.setText("キャンセル");
+                btnAction.setOnClickListener(onDecline);
                 break;
         }
     }
@@ -208,9 +234,6 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
                     JSONObject data = joresult.getJSONObject("data");
                     JSONObject ret = data.getJSONObject("ret");
                     GD.g_selectedSession = GD.sessionFromJson(ret);
-                    int status = ret.getInt("status");
-                    int action = ret.getInt("action");
-                    processStatus(status, action);
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -220,6 +243,7 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
         protected void onPostExecute(String result) {
             dialog.dismiss();
             super.onPostExecute(result);
+            processStatus(GD.g_selectedSession.status, GD.g_selectedSession.action);
         }
     }
 
@@ -246,9 +270,6 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
                     JSONObject data = joresult.getJSONObject("data");
                     JSONObject ret = data.getJSONObject("ret");
                     GD.g_selectedSession = GD.sessionFromJson(ret);
-                    int status = ret.getInt("status");
-                    int action = ret.getInt("action");
-                    processStatus(status, action);
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -258,6 +279,7 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
         protected void onPostExecute(String result) {
             dialog.dismiss();
             super.onPostExecute(result);
+            processStatus(GD.g_selectedSession.status, GD.g_selectedSession.action);
         }
     }
 
@@ -297,14 +319,15 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
         protected void onPostExecute(String result) {
             dialog.dismiss();
             super.onPostExecute(result);
+            processStatus(GD.g_selectedSession.status, GD.g_selectedSession.action);
         }
     }
 
     public class RefreshSessionApi extends AsyncTask<String, String, String> {
         @Override
         protected void onPreExecute() {
-            dialog.setCanceledOnTouchOutside(false);
-            dialog.show();
+//            dialog.setCanceledOnTouchOutside(false);
+//            dialog.show();
             super.onPreExecute();
         }
         protected String doInBackground(String... param) {
@@ -320,7 +343,22 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
                 boolean success = joresult.getBoolean("success");
                 if(success) {
                     JSONObject data = joresult.getJSONObject("data");
-                    JSONObject ret = data.getJSONObject("ret");
+                    JSONArray ret = data.getJSONArray("ret");
+                    JSONObject session = data.getJSONObject("session");
+                    GD.g_selectedSession = GD.sessionFromJson(session);
+                    messageNews = new ArrayList<>();
+                    for(int i = 0; i < ret.length(); i++){
+                        MessageModel messageModel = new MessageModel();
+                        JSONObject jtem = ret.getJSONObject(i);
+                        messageModel.id = jtem.getInt("id");
+                        messageModel.session_id = jtem.getInt("session_id");
+                        messageModel.from_to = jtem.getInt("from_to");
+                        messageModel.message = jtem.getString("message");
+                        messageModel.status = jtem.getInt("status");
+                        messageModel.user_image = jtem.getString("user_image");
+                        messageModels.add(messageModel);
+                        messageNews.add(messageModel);
+                    }
                 }
             } catch (JSONException e) {
                 e.printStackTrace();
@@ -328,8 +366,13 @@ public class FragmentSessionMessage extends FragmentParent implements View.OnCli
         }
         @Override
         protected void onPostExecute(String result) {
-            dialog.dismiss();
+//            dialog.dismiss();
             super.onPostExecute(result);
+            for(int i = 0; i < messageNews.size(); i++){
+                addMessage(messageNews.get(i));
+            }
+            runTimer();
+            processStatus(GD.g_selectedSession.status, GD.g_selectedSession.action);
         }
     }
 }
